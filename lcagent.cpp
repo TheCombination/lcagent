70e00691a04d154f790cd6fbadcd3b38b99f3c25
MarlzRana <77016115+MarlzRana@users.noreply.github.com>
2023-08-05 18:52:26 +0100

    Ignore the first argument (file path of the executable)

--- lcagent.cpp
+++ lcagent.cpp
@@ -1,5 +1,10 @@
 #include <cstdlib>
+#include <iostream>
 
-int main(){	
+int main(int argc, char** argv){
+	// Ignore the first argument (the file path of the executable)
+	--argc;
+	++argv;
+	
 	return EXIT_SUCCESS;
 }
