c71e9e693877b200d3a15659ee1ab84f3c2d85fc
MarlzRana <77016115+MarlzRana@users.noreply.github.com>
2023-08-06 20:29:11 +0100

    Introduce init subcommand with directory check

--- lcagent.cpp
+++ lcagent.cpp
@@ -1,42 +1,61 @@
 #include <cstdlib>
+#include <filesystem>
 #include <iostream>
+#include <string>
+#include <string_view>
 
-void add(int argc, char** argv);
+int init(std::string_view LCAGENT_DIR);
+void add(int argc, char **argv);
 void help();
 
-int main(int argc, char** argv){
+int main(int argc, char **argv) {
 	// Ignore the first argument (the file path of the executable)
 	--argc;
 	++argv;
-	
+
 	// Ensures there is at least one argument
 	if (argc < 1) {
 		std::cerr << "lcagent: I require at least one argument.\n"
-			 << "lcagent: Try the command \"lcagent help\" for more information on what I can do for you!\n";
+				  << "lcagent: Try the command \"lcagent help\" for more information on what I can do for you!\n";
 		return EXIT_FAILURE;
 	}
 
 	std::string subCommand{argv[0]};
-	
+
 	// Now, ignore the sub-command
-	--argc; 
+	--argc;
 	++argv;
 
-	if (subCommand == "add") {
- 		add(argc, argv);
-	} else if (subCommand  == "help") {
+	// Grab the necessary environment variables
+	std::string ENV_PWD{std::filesystem::current_path()};
+
+	// Produce the necessary shared metadata
+	std::string LCAGENT_DIR = ENV_PWD + "/.lcagent";
+
+	if (subCommand == "init") {
+		return init(LCAGENT_DIR);
+	} else if (subCommand == "add") {
+		add(argc, argv);
+	} else if (subCommand == "help") {
 		help();
 	} else {
-		std::cerr << "lcagent: I don't quite recognize that sub-command \"" << subCommand << "\"! \n" 
+		std::cerr << "lcagent: I don't quite recognize that sub-command \"" << subCommand << "\"! \n"
 				  << "lcagent: Try the command \"lcagent help\" to see a list of sub-commands I support!\n";
 		return EXIT_FAILURE;
 	}
-		
+
 	return EXIT_SUCCESS;
 }
 
-void add(int argc, char** argv) {}
+int init(std::string_view LCAGENT_DIR) {
+	if (std::filesystem::exists(LCAGENT_DIR) && std::filesystem::is_directory(LCAGENT_DIR)) {
+		std::cerr << "lcaagent: The lcagent is already initialized in your current directory!\n";
+		return EXIT_FAILURE;
+	}
 
-void help(){}
+	return EXIT_SUCCESS;
+}
 
+void add(int argc, char **argv) {}
 
+void help() {}
